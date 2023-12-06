c72cc21cd3880a9b2563570c5d9125b83552e823
Vinícius Souza <tradutorvinicius@gmail.com>
2023-12-06 14:59:30 -0300

    Resolve requisito 2

--- news/models.py
+++ news/models.py
@@ -6,3 +6,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
