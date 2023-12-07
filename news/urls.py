9934f273e8602656ec368973f812cc2d524fc17a
Vinícius Souza <tradutorvinicius@gmail.com>
2023-12-07 15:17:37 -0300

    Resolve requisito 5

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, news_details
 
 
 urlpatterns = [
     path('', home, name='home-page'),
+    path('news/<int:id>', news_details, name='news-details-page')
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import News
 
 # Create your views here.
@@ -6,3 +7,8 @@ from .models import News
 def home(request):
     context = {'news': News.objects.all()}
     return render(request, 'home.html', context)
+
+
+def news_details(request, id):
+    context = {'new': get_object_or_404(News, pk=id)}
+    return render(request, 'news_details.html', context)
