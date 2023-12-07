6ed6bdb40ed96669e28dcc8f205174a5fa9e0561
Vinícius Souza <tradutorvinicius@gmail.com>
2023-12-07 15:46:30 -0300

    Resolve requisito 8

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from news_rest.views.category_view import CategoryViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r"categories", CategoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include("news.urls")),
+    path("api/", include(router.urls))
 ]
 
 if settings.DEBUG:
